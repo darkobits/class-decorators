7dba91764729b3377e41c0219a6578e4014c3842
Joshua <joshua@collectivehealth.com>
2018-06-12 22:55:35 -0700

    build: Export type declarations.

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,10 @@
-export {default as default} from 'lib/class-decorator';
-export {default as ClassDecorator} from 'lib/class-decorator';
-export {default as MethodDecorator} from 'lib/method-decorator';
+// N.B. THIS FILE MUST USE RELATIVE IMPORT STATEMENTS OR TYPE DECLARATIONS WILL
+// NOT BE GENERATED CORRECTLY.
+
+// Values.
+export {default as default} from './lib/class-decorator';
+export {default as ClassDecorator} from './lib/class-decorator';
+export {default as MethodDecorator} from './lib/method-decorator';
+
+// Types.
+export * from './etc/types';
